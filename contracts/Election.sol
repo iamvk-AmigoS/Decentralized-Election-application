9ed32b2e366ff549c79309c24e127aeb46a5e780
iamvk-aitian <vishalkrishan4542@gmail.com>
2019-06-23 17:19:20 +0530

    3_Candidates_achieved

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,27 @@
 pragma solidity ^0.5.0;
 
 contract Election	{
-	//Store candidate
-	//read candidate
-	string public candidate;
-	//Constructor
-	constructor() public {
-		candidate = "Candidate 1";
+	//model a candidate
+	struct Candidate{
+		uint id;
+		string name;
+		uint voteCount;
 	}
 
+	//store candidate
+	//fetch candidate
+	mapping(uint => Candidate) public candidates;
+	//store candidate count
+	uint public candidatesCount;
+
+	constructor() public {
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+		addCandidate("Candidate 3");
+	}
+
+	function addCandidate (string memory _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
 }
