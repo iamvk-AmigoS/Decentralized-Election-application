d668265f93cbf22becf27ee00428cf2b00023b33
iamvk-aitian <vishalkrishan4542@gmail.com>
2019-06-23 16:32:14 +0530

    candidate_1_achieved

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract Election	{
+	//Store candidate
+	//read candidate
+	string public candidate;
+	//Constructor
+	constructor() public {
+		candidate = "Candidate 1";
+	}
+
+}
